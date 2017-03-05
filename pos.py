ffc5b53918bc78359c4d3b488bfff631b9e2c587
Administrator <lukas@stylianou.info>
2017-03-05 15:36:34 +0000

    bisschen was geschafft

--- pos.py
+++ pos.py
@@ -1,6 +1,5 @@
 from nltk.corpus import brown
 from nltk.util import ngrams
-#from collections import defaultdict
 from nltk.probability import FreqDist
 
 
@@ -9,54 +8,69 @@ from nltk.probability import FreqDist
 start = u'<s>'
 end = u'</s>'
 
-tag_sents = brown.tagged_sents()
-#tagset = set([t for (_,t) in brown.tagged_words()[:50000]]) # spaeter aendern noch START UND END REIN
+sents = []
 
-bigrams = []
-unigrams = []
-tagwords = []
-
-##for sen in tag_sents[:500]:
-##    curr_sen = [(start,u'START')] + sen + [(end, u'END')]
-##    tagwords += [(tag,word) for (tag,word) in curr_sen]
+# noch end tags und so auf die words packen?
+#
+# Look for words that occur only once
+# replace them by artificial UNK
+#
+##fd_words = FreqDist(brown.tagged_words()[:5000])
 ##
-
-
-for sen in tag_sents[:500]:
-    tagwords += [(tag,word) for (tag,word) in sen]
-
-
-# trenne die woerter je nach dem ob sie einmal oder nicht appearen
-fd_tag = FreqDist(brown.tagged_words())
-
-once = []
-notonce = []
-
-for e in fd_tag.iteritems():
-	if e[1]==1:
-		once.append(e[0])
-	else:
-		notonce.append(e)
-
-
-# for every tuple that appears once, replace the word by UNK
-##unklist = []
-##for e in once:
-##    unklist.append((u'UNK',e[1]))
+##once = set([e[0] for e in fd_words.iteritems() if e[1] == 1])
 ##
-##fd_unk = FreqDist(unklist)
-##
-##for e in fd_unk.iteritems():
-##    notonce.append(e)
+##for sen in brown.tagged_sents()[:5000]:
+##    tmp_sen = [(start,u'START')] # add start
+##    for wd in sen:
+##        if wd in once:
+##            tmp_sen.append((u'UNK',wd[1]))
+##        else:
+##            tmp_sen.append(wd)
+##    tmp_sen.append((end, u'END'))
+##    sents.append(tmp_sen)
 
-setonce = set(once)
+sents = brown.tagged_sents()[:5000]
 
-tag_sents_unk = []
 
-for sen in tag_sents:
+# frequency of single tags
+tag_uni = []
+tag_bi = []
+tag_wd = []
+
+# [[wd[1] for wd in sen] for sen in sents] 
+for sen in sents:
     for wd in sen:
-        if wd[0] in setonce:
-            wd = (u'UNK', wd[1])
+        tag_uni.append(wd[1]) # tag only
+
+fd_uni = FreqDist(tag_uni)
+
+# freqencies of tag bigrams
+for sen in sents:
+    tags_only = [wd[1] for wd in sen]
+    tag_bi+=ngrams(tags_only,2)
+
+fd_bi = FreqDist(tag_bi)
+
+# frequencies of the words tag tuples
+for sen in sents:
+    for wd in sen:
+        tag_wd.append(wd)
+
+fd_wd = FreqDist(tag_wd)
+
+# get relative frequency distributions
+ptt = {}
+
+pwt = {}
+
+for bi in fd_bi.iteritems():
+    ptt[bi[0]] = bi[1]*1.0/fd_uni[bi[0][0]]
+
+for wd in fd_wd.iteritems():
+    pwt[wd[0]] = wd[1]*1.0/fd_uni[bi[0][0]]
+
+
+    
 
     
 
@@ -64,31 +78,8 @@ for sen in tag_sents:
 
 
 
-# get all tag unigrams and tag bigrams including start and end markers
-##for sen in tag_sents[:500]:
-##    curr_sen = [(start,'START')] + sen + [(end, 'END')]
-##    curr_tags = [t for (_,t) in curr_sen]
-##    bigrams += ngrams(curr_tags, 2)
-##    unigrams += curr_tags
-##    tagwords += [ (tag,word) for (tag,word) in curr_sen]
 
-    
-
-# calc the freqdists
-fd_uni = FreqDist(unigrams)
-fd_bi = FreqDist(bigrams)
-
-
-# smooth unknown words
-# transfer freqdist to lists
-##fd_uni_list = [(k,v) for k,v in fd_uni.iteritems()]
-##fd_bi_list = [(k,v) for k,v in fd_bi.iteritems()]
-##fd_tag_list = [(k,v) for k,v in fd_tag.iteritems()]
-
-#x = [(k,v+1) for k,v in fd_tag.iteritems()]
 
 
 
-def getTransProb(key):
-    return fd_bi[key]/1.0/fd_uni[key[0]]
-
+    
