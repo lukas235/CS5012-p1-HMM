d0a8fa7c0bbc9d0f29a01989d7985e47f18da174
Administrator <lukas@stylianou.info>
2017-03-11 16:56:59 +0000

    commenting

--- tagger.py
+++ tagger.py
@@ -1,13 +1,18 @@
 from nltk.corpus import brown
 from nltk.util import ngrams
 from nltk.probability import FreqDist
-import datetime
-import re
 from math import exp, log
 from sys import float_info
+import datetime
+import re
 import tagset
 
 
+# Initiate the training sequence
+# m: start sentence of the training set
+# n: end sentence of the training set
+# t: tagset which shall be used
+# s: selection mode of the tags from the original tagset t (see tagset.py)
 def train(m,n,t,s):
     global smode
     smode = s
@@ -20,48 +25,56 @@ def train(m,n,t,s):
     print "Preparing and re-tagging sentences..."
     global sents
     sents = prepareSents(m,n)
-    print "Extracting words..."
+    
+    print "Extracting word-tag tuples from sentences..."
     global words
     words = getWords(sents)
-    print "Getting tokens..."
+    
+    print "Extracting words from tuples..."
     global tokens
     tokens = getTokens(words)
-    print "Calculating Types..."
+    
+    print "Getting word types..."
     global types
     types = getTypes(tokens)
-    print "Counting Unigram Tags..."
+    
+    print "Counting tag unigrams..."
     global fd_uni
     fd_uni = getTagFreqDist(words)
+    
     print "Calculating Transition probabilities..."
     global trans
     trans = getTransitionProbs(sents, tags, fd_uni)
+    
     print "Calculating Emission probabilities..."
     global em
     em = getEmissionProbs(words, types, tags, fd_uni)
-    print "Creating dictionary of known words..."
+    
+    print "Creating lexicon of known words..."
     global known_wds
     known_wds = getKnownWords(types)
 
-
+# Preprocess sentences
+# 1. Modify tagset in terms of size and add start, and end tags
+# 2. Deal with unknown words by extracting the hapax legomena and replace every hapax legomenon in the training set with UNK
 def prepareSents(m,n):
     tmp_sents = brown.tagged_sents()[m:n]
     sents = []
-    # append start and and tags
     for sen in tmp_sents:
         sents += [[('<s>', 'START')] + tagset.updatetags(sen,smode) + [('</s>', 'END')]]
 
-    # smooth unknown words
-    # first convert sentences to words
+    # handle unknown words
+    # first convert sentences to words and only get the word itself from the tuple
     tokens = []
     for sen in sents:
         for wd in sen:
             tokens.append(wd[0])
 
-    # get a freqdist & calc the hapax legomena
+    # calculate a freqdist & calc the hapax legomena
     fd_tok = FreqDist(tokens)
     hpx_lg = set([e[0] for e in fd_tok.items() if e[1] == 1])
 
-    # update sentences by adding the UNK word
+    # update sentences by replacing all words with UNK that are in the hpx_lg set
     new_sents = []
     for sen in sents:
         tmp_sen = []
@@ -73,6 +86,7 @@ def prepareSents(m,n):
         new_sents += [tmp_sen]
     return new_sents
 
+# Return (word, tag) tuples from sentences
 def getWords(sents):
     words = []
     for sen in sents:
@@ -80,45 +94,47 @@ def getWords(sents):
             words.append(wd)
     return words
 
+# Return word-only tokens from (word, tag) tuples
 def getTokens(words):
-    # create a list of types
     tokens = []
     for wd in words:
         tokens.append(wd[0])
     return tokens
 
+# Create a set of types from the tokens
 def getTypes(tokens):
-    # create a set of types
     return set(tokens)
 
+# Get absolute frequency distribution of the tag unigrams
 def getTagFreqDist(words):
     tag_uni = []
     for wd in words:
         tag_uni.append(wd[1])
     return FreqDist(tag_uni)
 
+# Return the lexicon of known words
 def getKnownWords(types):
 ##    known_words = types.copy()
     known_words = types
     known_words.remove('UNK')
     return known_words
 
-    # get the freqdist of the unigrams in the corpus by iterating through every word in every sentence
-    # and copying it into a list
-
-    # get the freqdist of the bigram tags in the corpus
+# Calculate the transition probabilities in the log domain
+# For the count of every tag bigram divide through the unigram count of the first tag in the tuple
+# Smooth with LaPlace
 def getTransitionProbs(sents, tags, fd_uni):
     tag_bi = []
     for sen in sents:
         tags_only = [wd[1] for wd in sen]
         tag_bi += ngrams(tags_only, 2)
-
     fd_bi = FreqDist(tag_bi)
 
+    # Initialise transitions table
     trans = {}
     for t1 in tags:
         for t2 in tags:
             trans[(t1, t2)] = log(float_info.min)
+
     # calculate transition probabilities & add LaPlace smoothing
     for t1 in tags:
         for t2 in tags:
@@ -129,16 +145,18 @@ def getTransitionProbs(sents, tags, fd_uni):
 
     
 
-    # get the freqdist of the word-tag tuples
+# Calculate the emission probabilities in the log domain
+# For the count of every word-tag tuple divide by the count of the tag unigram
 def getEmissionProbs(words, types, tags, fd_uni):
     fd_wd = FreqDist(words)
-    
+
+    # Initialise emissions table
     em = {}
     for wd in types:
         for tag in tags:
             em[(wd,tag)] = log(float_info.min)
 
-    # calculate emission probabilities
+    # calculate emission probabilities in the log domain
     for wd in fd_wd.items():
         prob = 1.0 * wd[1] / fd_uni[wd[0][1]]
         if prob != 0:
@@ -146,11 +164,10 @@ def getEmissionProbs(words, types, tags, fd_uni):
     return em
 
 
-# add sent before unknown
+# Implementation of the Viterbi algorithm as list of dicts
+# Every word is represented as an entry in a list with a dictionary of states (i.e. tags)
+# The viterbi algorithm is extended by checking if each word exists in the lexicon of known words
 def viterbi(sen, known_wds, states, p_trans, p_emit):
-    # Viterbi as list of dicts
-    # Each obs in the list has a dict of state dicts
-    # Each state dict has a prob and a backpointer
     V = [{}]
     out = []
 
@@ -170,39 +187,44 @@ def viterbi(sen, known_wds, states, p_trans, p_emit):
         for s in states:
             V[wd][s] = max(V[wd - 1][last_s] + p_trans[last_s,s] for last_s in states) + p_emit[(curr_wd, s)]
 
+    # terminate
+    maxptr = max(states, key=lambda last_s: V[-1][last_s] + p_trans[(last_s,'END')])
+
     # backtrack
-    maxptr = max(states, key=lambda last_s: V[len(sen)-1][last_s] + p_trans[(last_s,'END')])
     out += [(sen[len(sen) - 1], maxptr)]
     for wd in xrange(len(sen) - 1, 0, -1):
         maxptr = max(states, key=lambda last_s: V[wd-1][last_s] + p_trans[(last_s,maxptr)])
         out = [(sen[wd - 1], maxptr)] + out
     return out
 
+# Start tagging a 
 def tag(start,end):
-    ref = []
-    untagged = brown.sents()[start:end]
+    ref = [] # tagged reference sentences
+    untagged = brown.sents()[start:end] # untagged sentences which shall be tagged
+    
     for sen in brown.tagged_sents()[start:end]:
         ref += [tagset.updatetags(sen,smode)]
         
-        total_words = 0
-        right_words = 0
-        tagged_sents = []
+    total_words = 0
+    right_words = 0
+    tagged_sents = []
 
     start_time = datetime.datetime.now()
     print start_time
+
+    # Run Viterbi for every sentence in the test set
     for i in xrange(start,end):
         tagged_sentence = viterbi(untagged[i-start], types, tags, trans, em)
         reference = ref[i-start]
         tagged_sents += [tagged_sentence]
-        
+
+        # Count total words and right words
         for wd in xrange(0,len(tagged_sentence)):
             if tagged_sentence[wd][1] == reference[wd][1]:
                 right_words += 1
-##            else:
-##                print tagged_sentence[wd][1]
-##                print reference[wd][1]
             total_words += 1
 
+        
         print "{} / {}".format(i-start+1, end-start)
         print 1.0 * right_words/total_words
         totime = 100 * ((datetime.datetime.now()-start_time).seconds)/60 * (end-start)/(i-start+1)
@@ -210,18 +232,14 @@ def tag(start,end):
 
     end_time = datetime.datetime.now()
     print end_time
-    with open("small_tagset_.txt", "w") as f:
-        f.write(str(total_words))
-        f.write("|")
-        f.write(str(right_words))
-        f.write("|")
-        f.write(str(1.0 * right_words/total_words))
-        f.write("\n")
+    
+    # Save result as txt
+    with open("result.txt", "w") as f:
+        f.write("{}/{}|{}\n".format(str(right_words), str(total_words), str(1.0 * right_words/total_words)))
         f.write(str(start_time))
         f.write("\n")
         f.write(str(end_time))
         f.write("\n")
-
         for sen in tagged_sents:
             f.write("\n")
             f.write(str(sen))
